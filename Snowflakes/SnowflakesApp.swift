743fc7dd1b487dfd1b2d6f2106db36c2b11a770e
HtetAungShine6 <htetaungshine211299@gmail.com>
2024-11-04 12:58:45 +0700

    initial commit

---
+++ Snowflakes/SnowflakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowflakesApp.swift
+//  Snowflakes
+//
+//  Created by Htet Aung Shine on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowflakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
